9a9f80fea2c83ea8a47e0e7ea3a6d9db81022b8e
Emre Onder <emreo@backbase.com>
2023-08-22 16:32:03 +0200

    Refactor

--- Sources/SwiftUIBanner/BannerView.swift
+++ Sources/SwiftUIBanner/BannerView.swift
@@ -6,8 +6,8 @@ struct BannerView: View {
     var onDismiss: () -> Void
     
     @GestureState private var dragState = DragState.inactive
-    @State private var dragOffset: CGFloat = 0
-    @State private var isDragging = false // Track drag gesture state
+    @State private var timer: Timer?
+    @State private var isDragging: Bool = false // Track drag gesture state
     @EnvironmentObject private var bannerManager: BannerManager
     @Environment(\.safeAreaInsets) private var safeAreaInsets
 
@@ -17,61 +17,95 @@ struct BannerView: View {
     
     var body: some View {
         VStack {
-            HStack {
-                if let icon = style.icon {
-                    icon
-                        .font(.title)
-                        .foregroundColor(style.tintColor)
+            bannerContent
+                .padding(.horizontal, 16)
+                .padding(.top, safeAreaInsets.top)
+                .offset(y: dragState.translation.height)
+                .onAppear {
+                    startTimer()
                 }
-                VStack(alignment: .leading, spacing: 2) {
-                    Text(banner.title)
-                        .font(bannerManager.styles.titleFont)
-                        .foregroundColor(style.tintColor)
-                    if let subtitle = banner.subtitle {
-                        Text(subtitle)
-                            .font(bannerManager.styles.subtitleFont)
-                            .foregroundColor(style.tintColor)
-                    }
-                }
-                Spacer()
-            }
-            .padding(16)
-            .background(
-                style.backgroundColor
-                    .cornerRadius(bannerManager.styles.cornerRadius)
-                    .shadow(color: Color.black.opacity(0.1), radius: 20, x: 0, y: 15)
-            )
-            Spacer()
         }
-        .padding(.horizontal, 16)
-        .padding(.top, safeAreaInsets.top)
+        .contentShape(Rectangle())
+        .gesture(dragGesture)
         .transition(AnyTransition.asymmetric(insertion: .move(edge: .top).combined(with: .opacity),
                                              removal: .move(edge: .top).combined(with: .opacity)))
         .animation(.easeInOut)
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation(.easeInOut) {
-                    onDismiss()
-                }
-            }
-        }
         .id(banner.id)
+        .onChange(of: dragState) { newState in
+                    if case .inactive = newState {
+                        startTimer()
+                    } else {
+                        timer?.invalidate()
+                    }
+                }
     }
     
-    enum DragState {
+    private var bannerContent: some View {
+        HStack {
+            if let icon = style.icon {
+                icon
+                    .font(.title)
+                    .foregroundColor(style.tintColor)
+            }
+            VStack(alignment: .leading, spacing: 2) {
+                Text(banner.title)
+                    .font(bannerManager.styles.titleFont)
+                    .foregroundColor(style.tintColor)
+                if let subtitle = banner.subtitle {
+                    Text(subtitle)
+                        .font(bannerManager.styles.subtitleFont)
+                        .foregroundColor(style.tintColor)
+                }
+            }
+            Spacer()
+        }
+        .padding(16)
+        .background(
+            style.backgroundColor
+                .cornerRadius(bannerManager.styles.cornerRadius)
+                .shadow(color: Color.black.opacity(0.1), radius: 20, x: 0, y: 15)
+        )
+    }
+    
+    private func startTimer() {
+          timer?.invalidate()
+          timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+              if case .inactive = dragState {
+                  withAnimation(.easeInOut) {
+                      onDismiss()
+                  }
+              }
+          }
+      }
+    
+    private var dragGesture: some Gesture {
+        DragGesture()
+            .updating($dragState) { value, state, _ in
+                state = .dragging(translation: value.translation)
+            }
+            .onEnded { value in
+                let threshold = UIScreen.main.bounds.height * 0.25
+                if abs(value.translation.height) > threshold {
+                    withAnimation {
+                        onDismiss()
+                    }
+                }
+            }
+    }
+
+    enum DragState: Equatable {
         case inactive
         case dragging(translation: CGSize)
-        
+
         var translation: CGSize {
             switch self {
-            case .inactive, .dragging(translation: .zero):
+            case .inactive:
                 return .zero
             case .dragging(let translation):
                 return translation
             }
         }
     }
-    
 }
 
 struct BannerModifier: ViewModifier {
@@ -79,7 +113,7 @@ struct BannerModifier: ViewModifier {
     @State private var currentBanner: BannerData?
 
     func body(content: Content) -> some View {
-        ZStack {
+        ZStack(alignment: .top) {
             content
             if let banner = currentBanner {
                 BannerView(banner: banner) {
@@ -87,6 +121,7 @@ struct BannerModifier: ViewModifier {
                         currentBanner = nil
                     }
                 }
+                .zIndex(1)
                 .edgesIgnoringSafeArea(.top)
             }
         }
